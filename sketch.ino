f903acd82a499e37b4a7fe6f866ce5a279699fac
Vinicius <vinicius.oliveira100307@gmail.com>
2026-02-20 16:53:06 -0300

    codigo C++

---
+++ sketch.ino
@@ -0,0 +1,21 @@
+
+// C++ code
+int d0 = 0;
+
+void setup()
+{
+  pinMode(d0, OUTPUT);
+
+}
+
+void loop()
+{
+  
+  digitalWrite(d0, HIGH);
+  delay(1000);
+  digitalWrite(d0, LOW);
+  delay(1000);
+  
+}
+    
+ 
